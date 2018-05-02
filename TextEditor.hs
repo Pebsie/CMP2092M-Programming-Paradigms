f9ea41b10f1f48584b75def853f4a108d3fc3f12
Thomas Lock <email@peb.si>
2018-05-02 15:00:55 +0100

    Add highlighting

--- TextEditor.hs
+++ TextEditor.hs
@@ -6,35 +6,68 @@ data TextEditor = TextEditor { line :: String,
                                cursorPos :: Int,
                                highlightLeft :: Int,
                                highlightRight :: Int,
-                               clipboard :: String
+                               clipboard :: String,
+                               highlightOn :: Bool
                               } deriving (Show)
 
-displayText (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr}) = slice 0 (cp-1) l ++ ['|'] ++ cp `drop` l
+displayText (TextEditor {line = l, cursorPos = cp}) = slice 0 (cp-1) l ++ ['|'] ++ cp `drop` l
+displayHLText (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr}) = (slice 0 (hl-1) (displayText (TextEditor {line = l, cursorPos = cp}))) ++ ['['] ++ (slice hl (hr-1) (displayText (TextEditor {line = l, cursorPos = cp}))) ++ [']'] ++ (slice hr (length l) (displayText (TextEditor {line = l, cursorPos = cp})))
 
-intro = putStrLn "#### TEXT EDITOR ####\nType '/loadFile *filename*' to load a file.\nType '/saveFile *filename*' to save a file.\nType '/cursor left' or '/cursor right' to move the cursor.\nType '/highlight on' and '/highlight off' to start or stop highlighting.\nType '/delete' to delete everything that is currently highlighted.\nType '/cut', '/copy' and '/paste' to use those functions.\n\nEverything else will append the current line."
+intro = putStrLn "#### TEXT EDITOR ####\nType '/loadFile *filename*' to load a file.\nType '/saveFile *filename*' to save a file.\nType '/cl' or '/cr' to move the cursor left and right.\nType '/highlight on' and '/highlight off' to start or stop highlighting.\nType '/delete' to delete everything that is currently highlighted or the previous character.\nType '/cut', '/copy' and '/paste' to use those functions.\n\nEverything else will append the current line."
 
 main = do
         intro
-        editor (TextEditor {line = "", cursorPos = 0, highlightLeft = 0, highlightRight = 0, clipboard = ""})
+        editor (TextEditor {line = "", cursorPos = 0, highlightLeft = 0, highlightRight = 0, clipboard = "", highlightOn = False})
 
-editor (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr, clipboard = clip}) = do { --in order to be able to change values within the editor we need to use recursion, changing the parameter each time
-  putStrLn (displayText (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr}));
+editor (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = ho}) = do { --in order to be able to change values within the editor we need to use recursion, changing the parameter each time
+  if ho == False
+    then do putStrLn (displayText (TextEditor {line = l, cursorPos = cp}));
+  else
+    putStrLn (displayHLText (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr}));
+
   i <- getLine;
-  if i == "/cursor left"
+  if i == "/cl"
     then do
-      editor (TextEditor {line = l, cursorPos = (pred cp), highlightLeft = hl, highlightRight = hr, clipboard = clip});
-  else if i == "/cursor right"
+      if ho == True && (pred cp) < hl
+        then do
+          editor (TextEditor {line = l, cursorPos = (pred cp), highlightLeft = (pred cp), highlightRight = hr, clipboard = clip, highlightOn = ho});
+      else
+        editor (TextEditor {line = l, cursorPos = (pred cp), highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = ho});
+
+  else if i == "/cr"
     then do
-      editor (TextEditor {line = l, cursorPos = (succ cp), highlightLeft = hl, highlightRight = hr, clipboard = clip});
+
+      if ho == True && (succ cp) > hl
+        then do
+          editor (TextEditor {line = l, cursorPos = (succ cp), highlightLeft = hl, highlightRight = (succ cp), clipboard = clip, highlightOn = ho});
+      else
+        editor (TextEditor {line = l, cursorPos = (succ cp), highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = ho});
+
   else if i == "/quit"
     then do
       return ();
+  else if i == "/delete"
+    then do
+      if ho == True
+        then do
+          editor (TextEditor {line = ((slice 0 (hl-1) l) ++ (hr `drop` l)), cursorPos = (length (slice 0 (hl-1) l)), highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = False});
+      else
+        editor (TextEditor {line = ((slice 0 (cp-1) l) ++ ((cp+1) `drop` l)), cursorPos = cp, highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = False});
+
+  else if i == "/highlight on"
+    then do
+      putStrLn "Highlight mode is on.";
+      editor (TextEditor {line = l, cursorPos = cp, highlightLeft = cp, highlightRight = hr, clipboard = clip, highlightOn = True});
+  else if i == "/highlight off"
+    then do
+      putStrLn "Highlight mode is off.";
+      editor (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = False});
   else if head i == '/' --we don't want to insert into the line text that is a mistyped command
     then do
       putStrLn "Unknown command.";
-      editor (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr, clipboard = clip});
+      editor (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = ho});
   else
-    do editor (TextEditor {line = slice 0 (cp-1) l ++ i ++ cp `drop` l, cursorPos = length (slice 0 (cp-1) l ++ i ++ cp `drop` l), highlightLeft = hl, highlightRight = hr, clipboard = clip});
+    do editor (TextEditor {line = slice 0 (cp-1) l ++ i ++ cp `drop` l, cursorPos = (cp + length i), highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = ho});
 
 }
 
