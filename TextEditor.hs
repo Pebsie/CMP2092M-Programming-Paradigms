4365249dfd4f2f19012a3d3e196a49ee4564b47a
Thomas Lock <email@peb.si>
2018-05-02 16:35:17 +0100

    Add copy, cut and paste functions

--- TextEditor.hs
+++ TextEditor.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import System.IO
+import Control.Monad --for file reading and saving
+
 slice from to x = take (to - from + 1)(drop from x) --slice an array and return values from index to to index from
 
 data TextEditor = TextEditor { line :: String,
@@ -13,7 +16,8 @@ data TextEditor = TextEditor { line :: String,
 displayText (TextEditor {line = l, cursorPos = cp}) = slice 0 (cp-1) l ++ ['|'] ++ cp `drop` l
 displayHLText (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr}) = (slice 0 (hl-1) (displayText (TextEditor {line = l, cursorPos = cp}))) ++ ['['] ++ (slice hl (hr-1) (displayText (TextEditor {line = l, cursorPos = cp}))) ++ [']'] ++ (slice hr (length l) (displayText (TextEditor {line = l, cursorPos = cp})))
 
-intro = putStrLn "#### TEXT EDITOR ####\nType '/loadFile *filename*' to load a file.\nType '/saveFile *filename*' to save a file.\nType '/cl' or '/cr' to move the cursor left and right.\nType '/highlight on' and '/highlight off' to start or stop highlighting.\nType '/delete' to delete everything that is currently highlighted or the previous character.\nType '/cut', '/copy' and '/paste' to use those functions.\n\nEverything else will append the current line."
+
+intro = putStrLn "#### TEXT EDITOR ####\nType '/loadfile to load a file.\nType '/saveFile *filename*' to save a file.\nType '/cl' or '/cr' to move the cursor left and right.\nType '/highlight on' and '/highlight off' to start or stop highlighting.\nType '/delete' to delete everything that is currently highlighted or the previous character.\nType '/cut', '/copy' and '/paste' to use those functions.\n\nEverything else will append the current line."
 
 main = do
         intro
@@ -57,11 +61,37 @@ editor (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight
   else if i == "/highlight on"
     then do
       putStrLn "Highlight mode is on.";
-      editor (TextEditor {line = l, cursorPos = cp, highlightLeft = cp, highlightRight = hr, clipboard = clip, highlightOn = True});
+      editor (TextEditor {line = l, cursorPos = cp, highlightLeft = cp, highlightRight = succ cp, clipboard = clip, highlightOn = True});
   else if i == "/highlight off"
     then do
       putStrLn "Highlight mode is off.";
       editor (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = False});
+  else if i == "/cut"
+    then do
+      if ho == True
+        then do
+          putStrLn ("Copied '" ++ (slice hl (hr-1) l) ++ "' to clipboard.");
+          editor (TextEditor {line = ((slice 0 (hl-1) l) ++ (hr `drop` l)), cursorPos = (length (slice 0 (hl-1) l)), highlightLeft = hl, highlightRight = hr, clipboard = (slice hl (hr-1) l), highlightOn = False});
+      else --prevent user errors
+        putStrLn "Nothing is highlighted.";
+        editor (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = ho});
+  else if i == "/copy"
+    then do
+      if ho == True
+        then do
+          putStrLn ("Copied '" ++ (slice hl (hr-1) l) ++ "' to clipboard.");
+          editor (TextEditor {line = l, cursorPos = (length (slice 0 (hl-1) l)), highlightLeft = hl, highlightRight = hr, clipboard = (slice hl (hr-1) l), highlightOn = False});
+      else
+        putStrLn "Nothing is highlighted.";
+        editor (TextEditor {line = l, cursorPos = cp, highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = ho});
+  else if i == "/paste"
+    then do
+      editor (TextEditor {line = slice 0 (cp-1) l ++ clip ++ cp `drop` l, cursorPos = (cp + length clip), highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = ho});
+  else if i == "/loadfile"
+    then do
+      putStrLn "Enter filename with extension.";
+      x <- getLine;
+      editor (TextEditor {line = x, cursorPos = cp, highlightLeft = hl, highlightRight = hr, clipboard = clip, highlightOn = ho});
   else if head i == '/' --we don't want to insert into the line text that is a mistyped command
     then do
       putStrLn "Unknown command.";
